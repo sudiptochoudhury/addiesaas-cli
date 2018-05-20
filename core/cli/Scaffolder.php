7ecf4a864c6b1089f31be2003cdd4bacb9200bc6
SudiptoChoudhury <mail@sudipto.net>
2018-05-21 01:55:35 +0530

    base Scaffolder class added

---
+++ core/cli/Scaffolder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Addiesaas\Core\Support\CLI;
+
+use splitbrain\phpcli\CLI;
+use splitbrain\phpcli\Options;
+
+class Scaffolder extends CLI
+{
+    // register options and arguments
+    protected function setup(Options $options)
+    {
+        $options->setHelp('A very minimal example that does nothing but print a version');
+        $options->registerOption('version', 'print version', 'v');
+    }
+
+    // implement your code
+    protected function main(Options $options)
+    {
+        if ($options->getOpt('version')) {
+            $this->info('1.0.0');
+        } else {
+            echo $options->help();
+        }
+    }
+}
